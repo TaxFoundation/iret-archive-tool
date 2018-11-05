5b5e50c09244869fdbb1b487aed3eb6f00594674
Tom VanAntwerp <tom@tomvanantwerp.com>
2018-11-05 17:26:59 -0500

    List styles and font styles

--- src/App.js
+++ src/App.js
@@ -12,19 +12,55 @@ const indexName = 'iret';
 
 const searchClient = algoliasearch(appId, searchKey);
 
+const StyledApp = styled.div`
+  font-family: 'Lato', sans-serif;
+`;
+
 const StyledHits = styled(Hits)`
+  ul,
   li {
-    list-style: none;
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+
+  li {
+    > div {
+      border-bottom: 1px solid #ccc;
+      border-left: 1px solid #ccc;
+      border-right: 1px solid #ccc;
+      list-style: none;
+    }
+
+    &:first-child {
+      > div {
+        border-top: 1px solid #ccc;
+        border-top-left-radius: 4px;
+        border-top-right-radius: 4px;
+      }
+    }
+
+    &:last-child {
+      > div {
+        border-bottom-left-radius: 4px;
+        border-bottom-right-radius: 4px;
+      }
+    }
   }
 `;
 
 const StyledHit = styled.div`
   background-color: #fff;
+  padding: 0.5rem;
   position: relative;
   transition: background-color 0.2s ease-in-out;
 
   &:hover {
     background-color: #e6f4ff;
+  }
+
+  p {
+    margin: 0;
   }
 
   em.ais-Highlight-highlighted {
@@ -50,13 +86,15 @@ const Entry = props => {
         <Highlight attribute="title" hit={props.hit} />
       </p>
       <p>
-        <Highlight attribute="subtitle" hit={props.hit} />
-      </p>
-      <p>
-        <Highlight attribute="date" hit={props.hit} />
+        <em>
+          <Highlight attribute="subtitle" hit={props.hit} />
+        </em>
       </p>
       <p>
         <Highlight attribute="authors" hit={props.hit} />
+      </p>
+      <p>
+        <Highlight attribute="date" hit={props.hit} />
       </p>
       {props.hit.file ? <Link href={fileRoot + props.hit.file} rel="noopener noreferrer" target="_blank" /> : null}
     </StyledHit>
@@ -66,11 +104,13 @@ const Entry = props => {
 class App extends Component {
   render() {
     return (
-      <InstantSearch searchClient={searchClient} indexName={indexName}>
-        <SearchBox />
-        <StyledHits hitComponent={Entry} />
-        <Pagination />
-      </InstantSearch>
+      <StyledApp>
+        <InstantSearch searchClient={searchClient} indexName={indexName}>
+          <SearchBox />
+          <StyledHits hitComponent={Entry} />
+          <Pagination />
+        </InstantSearch>
+      </StyledApp>
     );
   }
 }
