c5e3cb47d5abf5f4107efdd06f3e805c235f9e67
Tom VanAntwerp <tom@tomvanantwerp.com>
2018-11-07 14:34:39 -0500

    Try pym again

--- public/pymSetup.js
+++ public/pymSetup.js
@@ -1,1 +1,3 @@
-var pymParent = new pymParent('iret', 'https://upbeat-dubinsky-cd5ae5.netlify.com/index.html', {})
+window.addEventListener('load', function() {
+  var pymParent = new pymParent('iret', 'https://upbeat-dubinsky-cd5ae5.netlify.com/index.html', {})
+});
