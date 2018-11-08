167529aa613513dd674a3bb07411ee7d956b279a
Tom VanAntwerp <tom@tomvanantwerp.com>
2018-11-08 16:36:00 -0500

    Blank dropdown default option

--- src/components/DropdownRefinement.js
+++ src/components/DropdownRefinement.js
@@ -4,6 +4,7 @@ import { connectRefinementList } from 'react-instantsearch-dom';
 const DropdownRefinement = ({ currentRefinement, refine, items }) => {
   return (
     <select value={currentRefinement[0]} onChange={e => refine(e.target.value)}>
+      <option value="">--</option>
       {items.map(item => (
         <option key={item.label} value={item.label}>
           {item.label}
